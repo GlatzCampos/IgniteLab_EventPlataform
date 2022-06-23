13c67255bd379f56a2217d82d62d28384c8844e3
Gabriel Glatz <gabrielglatzc@gmail.com>
2022-06-22 22:19:08 -0300

    chore: created page components

---
+++ src/components/Video.tsx
@@ -0,0 +1,9 @@
+import { disableExperimentalFragmentVariables } from "@apollo/client";
+
+export function Video(){
+    return (
+        <div className="flex-1">
+            Video
+        </div>
+    )
+}
