9ef9080bdbc21f4abe4ff62b52b9c26d44913df5
Gabriel Glatz <gabrielglatzc@gmail.com>
2022-06-24 01:22:04 -0300

    feat: imported apollo_provider | router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import { Event } from "./pages/Event"
+import { BrowserRouter } from "react-router-dom"
+import { ApolloProvider } from "@apollo/client"
+import { client } from "./lib/apollo"
+import { Router } from "./Router"
 
 function app(){
   return(
-    <Event />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ApolloProvider>
+    
   )
 }
 
